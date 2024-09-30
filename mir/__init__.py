6f93c4dc61fd46d75c9e4a8d6999d671ed7118ca
Ettore Ricci <minecraft.etto@gmail.com>
2024-09-30 15:51:15 +0200

    updated project dir and project update method

--- mir/__init__.py
+++ mir/__init__.py
@@ -1,3 +1,12 @@
 import os
-PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+if os.getenv("COLAB_RELEASE_TAG"):
+    COLAB = True
+else:
+    COLAB = False
+    
+if COLAB:
+    PROJECT_DIR = os.path.abspath("./MIRProject")
+else:
+    PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(PROJECT_DIR, 'data')
