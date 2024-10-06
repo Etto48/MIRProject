a3290146843ebe2d572b88d21616c26008e5ed2c
Ettore Ricci <minecraft.etto@gmail.com>
2024-10-06 17:32:41 +0200

    Added todo and tests

---
+++ mir/test.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestIR(unittest.TestCase):
+    # test function names must start with "test"
+    def test_dummy(self):
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
