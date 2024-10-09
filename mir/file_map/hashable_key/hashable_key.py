9cf19012947aba9d3ff84f687f813d6ee509288a
Ettore Ricci <minecraft.etto@gmail.com>
2024-10-10 00:22:59 +0200

    Removed redundant function in interface

--- mir/file_map/hashable_key/hashable_key.py
+++ mir/file_map/hashable_key/hashable_key.py
@@ -6,18 +6,14 @@ class HashableKey(ABC, Hashable):
     """
     This abstract class is used to define the interface for a hashable key.
     An hashable key is an object that can be used as a key in a FileHMap.
-    The object must implement the __hash__ and __bytes__ methods.
-    Also two HashableKey objects with the same __bytes__ must
-    produce the same hash.
     """
-    
-    @abstractmethod
-    def __hash__(self):
-        ...
     
     @abstractmethod
     def __bytes__(self):
         ...
+        
+    def __hash__(self):
+        return hash(self.__bytes__())
         
     def __eq__(self, other):
         return self.__bytes__() == other.__bytes__()
--- mir/file_map/hashable_key/impls/str_hk.py
+++ mir/file_map/hashable_key/impls/str_hk.py
@@ -5,8 +5,5 @@ class StrHK(HashableKey):
     def __init__(self, key: str):
         self.key = key
 
-    def __hash__(self):
-        return hash(self.key)
-
     def __bytes__(self):
         return self.key.encode()
