d92c06c64a5370ea3568b6c1dfcc5fa6efe76645
erni-de <erni.deliallisi00@gmail.com>
2024-10-11 19:10:29 +0200

    Changed docstring

--- mir/ir/token_ir.py
+++ mir/ir/token_ir.py
@@ -12,7 +12,8 @@ class Token:
     """
     Token class is just a dataclass to keep track of the
     position of the word in a given document and wether it's
-    present in the title or in the body (lyrics) of the song
+    present in the author's name, title or in the 
+    body (lyrics) of the song
     """
     token: str
     where: TokenLocation
