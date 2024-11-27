8edb6286c7c158f7d9b4fef291560bbc4c693636
elahe <z.omrani@studenti.unipi.it>
2024-11-27 16:36:59 +0100

    implemented bm25f

--- mir/ir/impls/bm25f_scoring.py
+++ mir/ir/impls/bm25f_scoring.py
@@ -6,7 +6,7 @@ import math
 
 
 class BM25FScoringFunction:
-    def __init__(self, k1: float = 1.5, b: float = 0.75, field_weights: Dict[str, float] = None):
+    def __init__(self, k1: float = 1.5, b: float = 0.75, field_weights: Dict[str, float] = None, index = None):
         """
         BM25F scoring function initialization.
 
@@ -17,6 +17,7 @@ class BM25FScoringFunction:
         """
         self.k1 = k1
         self.b = b
+        self.index = index
         # Set default field weights if none are provided
         self.field_weights = field_weights or {'title': 2.0, 'body': 1.0,'author': 0.5}
 
@@ -32,30 +33,30 @@ class BM25FScoringFunction:
         # Returns
         - float: The BM25F score of the document.
         """
-        score = 0.0
-        avg_field_lengths = kwargs['avgdfl']  # Assume document provides average field lengths
+        score = 0.0 #RSV
 
 
         for term in query:
-            weighted_tf = 0.0
-
-            # Sum the weighted term frequencies across all fields
-            for field, weight in self.field_weights.items():
-                # Assume each posting has a method to get term frequency for a given field
-                # ( I have to look for the posting corresponding to the term)
-                # the term frequency 
-
-                tf = 
-                field_length = document.get_field_length(field)
-                avg_length = avg_field_lengths.get(field, 1)  # Default to 1 if no avg length is found
-
-                # BM25F field-specific term frequency component
-                normalized_tf = tf / (1 + self.k1 * ((1 - self.b) + self.b * (field_length / avg_length)))
-                weighted_tf += weight * normalized_tf
-
-            # Add term contribution to the total score
-            score += term.idf * weighted_tf * (self.k1 + 1) / (weighted_tf + self.k1)
+           score += self._rsv(term,document,postings)
 
         return score
 
+    def _rsv(self,term,document,postings):
+        tfd = self._wtf(term,document,postings)
+        score = (tfd/(self.k1+tfd))*math.log(term.idf)
+        return score
 
+
+    def _wtf(self,term,document,postings):
+        tfd = 0
+        for field, weight in self.field_weights.items():
+            avg_dlf = self.index.get_global_info()['avg_field_lengths'][field]
+            bb = 1-self.b+self.b*document.get_field_length(field)/avg_dlf
+            # questo e sbagliato perche non e piena la lista di posting, ma e sparsa.
+            # quindi costruisci un dizionario {term.id: Posting for term in query}
+            # la costruzione del dizionario deve essere fatta nel costruttore.
+            # poi questa funzione dovrebbe essere corretta.
+            tf = len(postings[term.id].occurrences[field])
+            tfd += weight*tf/bb
+        return tfd
+    
