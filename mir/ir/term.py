8d947dc826cbba3b4736fa0f04566f1412fb6ca8
paolo-asus <paol.palumbo@gmail.com>
2024-12-06 11:06:51 +0100

    fixed an off by 1 in term deser

--- mir/ir/term.py
+++ mir/ir/term.py
@@ -14,17 +14,18 @@ class Term:
         term_bytes = self.term.encode('utf-8')
         term_len = len(term_bytes)
         idf = self.info['idf']
-        return struct.pack(f'ii{term_len}s', idf, term_len, term_bytes)
+        pack = struct.pack(f'i{term_len}si', term_len, term_bytes, idf)
+        return pack
     
     @staticmethod
     def __deser__(data: bytes, id:int) -> "Term":
-        idf = struct.unpack('i', data[:4])[0]
-        data = data[4:]
+        print(data, len(data))
         term_len = struct.unpack('i', data[:4])[0]
         data = data[4:]
         term = struct.unpack(f'{term_len}s', data[:term_len])[0]
         term = term.decode('utf-8')
-        data = data[term_len:]
+        data = data[term_len+1:] # +1 for the null byte
+        idf = struct.unpack('i', data[:4])[0]
         return Term(term, id, idf=idf)
     
 
